6e92130344ec156fb70eb6d756e86efdc54beb7f
chengzhaohui <chengzhaohui@zhihu.com>
2018-05-22 16:16:46 +0800

    Initial Commit

---
+++ openjpeg/openjpeg.h
@@ -0,0 +1,13 @@
+//
+//  openjpeg.h
+//  openjpeg
+//
+//  Created by chengzhao on 2018/5/22.
+//  Copyright © 2018年 zhihu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface openjpeg : NSObject
+
+@end
---
+++ openjpeg/openjpeg.m
@@ -0,0 +1,13 @@
+//
+//  openjpeg.m
+//  openjpeg
+//
+//  Created by chengzhao on 2018/5/22.
+//  Copyright © 2018年 zhihu. All rights reserved.
+//
+
+#import "openjpeg.h"
+
+@implementation openjpeg
+
+@end
